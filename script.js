afa67fc3a11b48b85a28915e1a0aaac5300f2a6d
Muhammad Effan Choirunanda <82354518+mhmmadeffan@users.noreply.github.com>
2025-07-14 17:05:49 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+// Dapatkan modal
+var modal = document.getElementById("imageModal");
+
+// Dapatkan gambar dan masukkan ke dalam modal, dan gunakan atribut "alt" sebagai teks keterangan
+var img = document.getElementById("calendarImage");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+var overlay = document.getElementById("overlay"); // Dapatkan elemen overlay
+
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+  overlay.classList.add("active"); // Aktifkan overlay
+}
+
+// Dapatkan elemen <span> yang menutup modal
+var span = document.getElementsByClassName("close")[0];
+
+// Fungsi untuk menutup modal
+function close_modal() {
+  modal.style.display = "none";
+  overlay.classList.remove("active"); // Nonaktifkan overlay
+}
+
+// Ketika pengguna mengklik <span> (x), tutup modal
+span.onclick = function() {
+  close_modal();
+}
+
+// Ketika pengguna mengklik di luar gambar modal (pada area modal itu sendiri), tutup modal juga
+modal.onclick = function(event) {
+    if (event.target === modal) {
+        close_modal();
+    }
+}
